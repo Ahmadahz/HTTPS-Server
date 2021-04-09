b7ac01ab6043ec92df316dfe67fd9b8c72a9fc08
leighton <latedog@g.ucla.edu>
2021-04-09 11:00:58 +0000

    Implement http request echoing
    Change-Id: I2b5420f2b93a93167dc110525b2bdbe7c34f859f

--- src/session.cc
+++ src/session.cc
@@ -31,10 +31,30 @@ void session::handle_read(const boost::system::error_code& error,
 {
   if (!error)
   {
-    boost::asio::async_write(socket_,
-        boost::asio::buffer(data_, bytes_transferred),
-        boost::bind(&session::handle_write, this,
-          boost::asio::placeholders::error));
+    std::string request = data_;
+    if (request.find("\r\n\r\n") == std::string::npos &&
+	request.find("\n\n") == std::string::npos) {
+      // Append data until double CRLF/LF is found.
+      // WARNING: Assumes the request is at most `max_length' bytes.
+      socket_.async_read_some(boost::asio::buffer(&data_[strlen(data_)], max_length),
+      boost::bind(&session::handle_read, this,
+        boost::asio::placeholders::error,
+        boost::asio::placeholders::bytes_transferred));
+    } else {
+      std::string response;
+      response = "HTTP/1.1 200 OK\r\n";
+      response += "Content-Type: text/plain\r\n";
+      response += "\r\n";
+      response += data_;
+      strncpy(data_, response.c_str(), response.size());
+    
+      boost::asio::async_write(socket_,
+	boost::asio::buffer(data_, response.size()),
+	boost::bind(&session::handle_write, this,
+	  boost::asio::placeholders::error));
+
+      memset(data_, '\0', sizeof(data_));
+    }
   }
   else
   {
