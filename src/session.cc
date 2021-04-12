2218c57de3671d9fe21e29780ee6a6a88e9593ae
leighton <latedog@g.ucla.edu>
2021-04-12 04:05:14 +0000

    Fixed http response on browser
    Change-Id: I4e7cfb3f7e032a35d845cff95b78078eab39559e

--- src/session.cc
+++ src/session.cc
@@ -26,12 +26,15 @@ void session::start() {
 void session::handle_read(const boost::system::error_code& error,
     size_t bytes_transferred) {
   if (!error) {
+    std::cerr << "In handle_read\n";
+    
     // Implements the basic http echo requests from assignment 2.
     // Uses double newline characters to detect end of requests.
     
     std::string request = data_;
     if (request.find("\r\n\r\n") == std::string::npos &&
         request.find("\n\n") == std::string::npos) {
+      std::cerr << "Appending data\n";
       // Append data until double CRLF/LF is found.
       // WARNING: Assumes the request is at most `max_length' bytes.
       socket_.async_read_some(boost::asio::buffer(&data_[strlen(data_)], max_length),
@@ -41,14 +44,20 @@ void session::handle_read(const boost::system::error_code& error,
     } 
     else {
       std::string response;
-      response = "HTTP/1.1 200 OK\r\n";
+      response += "    \n"; // This line here is needed because otherwise the
+                            // the HTTP header disappears in the browser.
+                            // Otherwise, it works in the terminal.
+      
+      response += "HTTP/1.1 200 OK\r\n";
       response += "Content-Type: text/plain\r\n";
       response += "\r\n";
       response += data_;
       strncpy(data_, response.c_str(), response.size());
+
+      std::cerr << response << std::endl;
     
       boost::asio::async_write(socket_,
-          boost::asio::buffer(data_, response.size()),
+        boost::asio::buffer(data_, strlen(data_)),
           boost::bind(&session::handle_write, this,
           boost::asio::placeholders::error));
 
@@ -63,12 +72,17 @@ void session::handle_read(const boost::system::error_code& error,
 void session::handle_write(const boost::system::error_code& error)
 {
   if (!error) {
+    std::cerr << "Attempting to read in handle_write\n";
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
         boost::bind(&session::handle_read, this,
           boost::asio::placeholders::error,
           boost::asio::placeholders::bytes_transferred));
+
+    std::cerr << "Past read_some in handle_write\n";
+    socket_.close();
   }
   else {
+    std::cerr << "Deleting in handle_write\n";
     delete this;
   }
 }
