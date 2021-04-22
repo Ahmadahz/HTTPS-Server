b981e22fd3c7d69b06a1290d321f7308b2aa4f1c
Ahmad Habibollahzadeh <ahmadhz@g.ucla.edu>
2021-04-22 00:34:48 -0700

    Completed the main handler
    Change-Id: Ifb523b771a5238951efe800ca8196bf6e5919d07

--- src/handler.cc
+++ src/handler.cc
@@ -1,12 +1,71 @@
 #include "handler.h"
+#include <regex>
 
 Request RequestHandler::parse_request(const char* request) {
   Request parsed_request;
   std::string req(request);
   parsed_request.content = req;
 
-  // Parse the request for type, file path, and, if applicable, extension type.
+  //Find the Request Type
+  std::string s1("static");
+  std::string s2("echo");
+  if (req.find(s1) != std::string::npos) {
+    parsed_request.type = RequestType::File;
+  }
+  else if (req.find(s2) != std::string::npos) {
+    parsed_request.type = RequestType::Echo;
+  }
+  else{
+	parsed_request.type = RequestType::Unknown;
+  }
   
-
+  //Find the path
+  if (parsed_request.type == RequestType::File){
+	std::string file_path;
+	for (std::string::size_type j = 4; j < req.size(); ++j) {
+		if (req[j] == ' ') { 
+			break;
+		}
+		else {
+			file_path = file_path + req[j];
+		}
+	}
+	
+	//Is this path correct?!?!?! 
+	file_path = std::regex_replace(file_path, std::regex("static"), "files/bar");
+	parsed_request.path = file_path;
+  }
+  
+  //Find the extension type
+  if (parsed_request.type == RequestType::File){
+	bool extensionset = false;
+	std::string extension;
+	for (std::string::size_type i = 5; i < req.size(); ++i) {
+		if (req[i] == ' ') { 
+			break;
+		}
+		else if (req[i] == '.') { 
+			extensionset = true; 
+		}
+		else if (extensionset) {
+			extension = extension + req[i];
+		}
+	}
+	
+	//Set the extension type
+	if (extension.compare("html") == 0){
+		parsed_request.ext = ExtType::HTML;
+    }
+	else if (extension.compare("jpg") == 0){
+       parsed_request.ext = ExtType::JPG; 
+    }
+	else if (extension.compare("zip") == 0){
+       parsed_request.ext = ExtType::ZIP; 
+    }
+	else if (extension.compare("txt") == 0){
+       parsed_request.ext = ExtType::TXT;  
+    }
+  }
+  
   return parsed_request;
 }
