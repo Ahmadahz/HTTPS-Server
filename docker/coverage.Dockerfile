6f3caaf825593595451292bb55e6a6dfc47dd0de
RAHUL CHIKKA <rchikka11@g.ucla.edu>
2021-05-10 16:29:53 +0000

    Revert "Reverted changes I did before back and trying to rename cloud build for coverage different from build for running"
    This reverts commit aaa451ed31d5657ba2305ec6ce29dcbd2ccd00e0.
    
    Reason for revert: Ahmad made similar modifications to the files and it's causing a merge conflict.
    
    Change-Id: Ia1da4b7a9cefa0a82a148d4551c18ae8d89ec922

--- docker/coverage.Dockerfile
+++ docker/coverage.Dockerfile
@@ -5,7 +5,7 @@ COPY . /usr/src/project
 WORKDIR /usr/src/project/build_coverage
 
 RUN pwd
-RUN cmake ..
-RUN make
+# RUN cmake ..
+# RUN make
 RUN cmake -DCMAKE_BUILD_TYPE=Coverage ..
 RUN make coverage
--- docker_build.sh
+++ docker_build.sh
@@ -2,7 +2,6 @@
 # rebuilds docker file
 docker build -f docker/base.Dockerfile -t lark:base .
 docker build -f docker/Dockerfile -t my_test . 
-docker build -f docker/coverage.Dockerfile -t my_coverage . 
 docker run --rm -p 127.0.0.1:80:80 --name my_run my_test:latest
 
 #To stop it later: docker container stop my_run
